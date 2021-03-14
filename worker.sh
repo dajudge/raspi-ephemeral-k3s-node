5e295ce6daf896e672b500b49d5d2cff1e64ec66
Alex Stockinger <mail@alexstockinger.de>
2021-03-14 15:42:04 +0100

    Use docker instead of containerd for agent

--- worker.sh
+++ worker.sh
@@ -12,4 +12,5 @@ NODE_NAME="raspi-$(echo $IP | sed 's/\\./_/g')"
 k3s agent --token $K3S_TOKEN \
   --server $K3S_URL \
   --node-ip $IP --node-external-ip $IP \
-  --node-name $NODE_NAME
+  --node-name $NODE_NAME \
+  --docker
