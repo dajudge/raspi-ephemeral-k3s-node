e008da898fac4d1d7bdbaa660d87b6c0f5a66740
Alex Stockinger <mail@alexstockinger.de>
2021-03-14 10:12:01 +0100

    Add startup script for worker

---
+++ worker.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+set -e
+
+DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
+
+if [[ INTERACTIVE == "true" ]]; then
+  MODE="--it"
+else
+  MODE="-d"
+fi
+
+IMG=$(docker build -q $DIR/worker)
+
+docker run --rm $MODE \
+  --tmpfs /etc/rancher \
+  --tmpfs /var/lib \
+  --tmpfs /var/run \
+  --tmpfs /var/log \
+  --tmpfs /tmp \
+  --tmpfs /run \
+  --privileged \
+  --net host \
+  --read-only \
+  --env-file .env \
+  IMG
