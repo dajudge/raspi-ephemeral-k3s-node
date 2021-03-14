5111a9db62f5012127ae38208b76bf569bad611c
Alex Stockinger <mail@alexstockinger.de>
2021-03-14 15:44:25 +0100

    Simplify worker.sh

--- worker.sh
+++ worker.sh
@@ -7,7 +7,7 @@ DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 . $DIR/.env
 
 IP=$(ip addr show dev $PUBLIC_INTERFACE | grep '^\s*inet\s' | awk '{print $2}' | sed 's/\/.*//')
-NODE_NAME="raspi-$(echo $IP | sed 's/\\./_/g')"
+NODE_NAME="raspi-$(echo $IP)"
 
 k3s agent --token $K3S_TOKEN \
   --server $K3S_URL \
